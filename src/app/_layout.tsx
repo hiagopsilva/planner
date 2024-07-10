461c5ab5b47a4c157de2414e759b3cfa0903368b
Hiago Prates <hiagohps360@gmail.com>
2024-07-10 00:19:27 -0300

    feat: status bar config

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 
 import '@/styles/global.css'
 import { Slot } from 'expo-router'
-import { View } from 'react-native'
+import { View, StatusBar } from 'react-native'
 
 const Layout: React.FC = () => {
   return (
     <View className="flex-1 bg-zinc-950">
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <Slot />
     </View>
   )
