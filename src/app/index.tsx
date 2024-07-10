fe6b529be0aeab65d8ed66931679fbd1b6cb0418
Hiago Prates <hiagohps360@gmail.com>
2024-07-10 00:18:15 -0300

    chore: text initial

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,9 @@ import { Text, View } from 'react-native'
 const Index: React.FC = () => {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text>Hello Planner</Text>
+      <Text className="text-zinc-400">
+        Convide seus amigos e planeje sua próxima viagem.
+      </Text>
     </View>
   )
 }
