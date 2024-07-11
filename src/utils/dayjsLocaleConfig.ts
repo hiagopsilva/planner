24bacab76fd9fa88b657a1a02387c631e3d5e9b9
Hiago Prates <hiagohps360@gmail.com>
2024-07-11 16:25:45 -0300

    fix: eslint

--- src/utils/dayjsLocaleConfig.ts
+++ src/utils/dayjsLocaleConfig.ts
@@ -1,5 +1,4 @@
-import "dayjs/locale/pt-br"
-import dayjs from "dayjs"
+import 'dayjs/locale/pt-br'
+import dayjs from 'dayjs'
 
-// Para traduzir os nomes de dias e mês para pt-br.
-dayjs.locale("pt-br")
+dayjs.locale('pt-br')
