3e370e319b38f64375d14e09fb2ebb7299b0749d
Hiago Prates <hiagohps360@gmail.com>
2024-07-18 20:43:52 -0300

    feat: input locale destination trip

--- src/app/trip/[id].tsx
+++ src/app/trip/[id].tsx
@@ -1,12 +1,75 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { Input } from '@/components/input'
+import Loading from '@/components/loading'
+import { TripDetails, tripServer } from '@/server/trip-server'
+import { colors } from '@/styles/colors'
+import dayjs from 'dayjs'
+import { router, useLocalSearchParams } from 'expo-router'
+import { MapPin, Settings2 } from 'lucide-react-native'
+import React, { useEffect, useState } from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
 
-// import { Container } from './styles';
+type TripData = TripDetails & {
+  when: string
+}
 
 const trip: React.FC = () => {
+  const [isLoadingTrip, setIsLoadingTrip] = useState(true)
+  const [tripDetails, setTripDetails] = useState({} as TripData)
+
+  const tripId = useLocalSearchParams<{ id: string }>().id
+
+  async function getTripDetails() {
+    try {
+      setIsLoadingTrip(true)
+
+      if (!tripId) {
+        return router.back()
+      }
+
+      const trip = await tripServer.getById(tripId)
+
+      const maxLengthDestination = 14
+      const destination =
+        trip.destination.length > maxLengthDestination
+          ? trip.destination.slice(0, maxLengthDestination) + '...'
+          : trip.destination
+
+      const starts_at = dayjs(trip.starts_at).format('DD')
+      const ends_at = dayjs(trip.ends_at).format('DD')
+      const month = dayjs(trip.starts_at).format('MMMM')
+
+      setTripDetails({
+        ...trip,
+        when: `${destination} de ${starts_at} a ${ends_at} de ${month}.`,
+      })
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoadingTrip(false)
+    }
+  }
+
+  useEffect(() => {
+    getTripDetails()
+  }, [])
+
+  if (isLoadingTrip) {
+    return <Loading />
+  }
+
   return (
-    <View>
-      <Text>TRIP</Text>
+    <View className="flex-1 px-5 pt-16">
+      <Input variant="tertiary" className="p-4 rounded">
+        <MapPin color={colors.zinc[400]} size={20} />
+        <Input.Field value={tripDetails.when} readOnly />
+
+        <TouchableOpacity
+          activeOpacity={0.6}
+          className="w-9 h-9 bg-zinc-800 items-center justify-center rounded"
+        >
+          <Settings2 color={colors.zinc[400]} size={20} />
+        </TouchableOpacity>
+      </Input>
     </View>
   )
 }
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -10,7 +10,6 @@ import {
 } from 'react-native'
 
 type InputProps = ViewProps & {
-  children: React.ReactNode
   variant?: 'primary' | 'secondary' | 'tertiary'
 }
 
